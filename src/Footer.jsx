59d78df171b15986b36ce2f40aa60c764d75c821
Yash Ojha <yashojha9033@gmail.com>
2024-12-30 12:08:21 +0530

    Responsive Changes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,14 +8,6 @@ function Footer(){
                     <div className="logo-wrap">
                         <img src={logo} alt="Website Logo" />
                     </div>
-                    <nav>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Services</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </nav>
                     <p>&copy; {new Date().getFullYear()} Day Organiser</p>
                 </div>
             </div>
